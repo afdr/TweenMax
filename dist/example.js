21f7a92a3be529cda9927eb3fc89a9da0cb37663
lmlm <18251937301@163.com>
2018-12-13 11:13:51 +0800

    again

---
+++ dist/example.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function run() {
+    var test = 1212;
+    console.log(test);
+}
+run();
+//# sourceMappingURL=example.js.map
---
+++ example.js
@@ -0,0 +1,5 @@
+function run (){
+    let test = 1212;
+    console.log(test);
+}
+run();
